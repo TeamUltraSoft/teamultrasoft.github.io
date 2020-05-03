a67f3d8dc499f33e75782a95a6f98cbd6e1f7017
TheKnightGamerZ <52719312+TheKnightGamerZ@users.noreply.github.com>
2020-05-04 02:39:53 +0300

    update

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+jQuery.fn.blindLeftToggle = function (duration, easing, complete) {
+    return this.animate({
+        marginLeft: parseFloat(this.css('marginLeft')) < 0 ? 0 : -this.outerWidth()
+    }, jQuery.speed(duration, easing, complete));
+};
+
+$(document).ready(function() {
+
+    $('.mobile-nav').click(function() {
+        var nav = $('.sidebar');
+        var icon = $('.mobile-nav i');
+    
+        nav.blindLeftToggle(500);
+    
+    });
+
+    $('.scroll-to').click(function() {
+        $('html, body').animate({scrollTop: $('#goto').offset().top}, 1000)
+    });
+
+});
